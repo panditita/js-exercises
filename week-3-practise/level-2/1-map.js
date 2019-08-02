1e74cbf73879440b91eb786049996500b5ee5e9e
panditita <waracleadmin@Rekhas-MacBook-Pro.local>
2019-08-02 15:40:33 +0100

    Working on week 3 practice

--- week-3-practise/level-2/1-map.js
+++ week-3-practise/level-2/1-map.js
@@ -12,10 +12,38 @@ var londonLocations = [
   ["Greenwich", "tube", "bus", "river boat"]
 ];
 
-var locationsByBoat;
+// First map, loops into the object and creates individual arrays
+var locationsByBoat = londonLocations.map(oneLocation => {
+  var visitLocations = [];
+  // Second map, loops into each element of each array
+  oneLocation.map(withBoat => {
+    // Then it compares each string agains 'boat' to find a match
+    var location = [];
+    if (withBoat.match("boat")) {
+      // if the comparison is valid then it stores that value into location and location into Visit
+      location.push(oneLocation[0]);
+      visitLocations.push(location.concat());
+    }
+  });
+  return visitLocations.filter(empty => {
+    return empty != null;
+  });
+});
 
 console.log(locationsByBoat);
 
 /* EXPECTED OUTPUT
 ["London bridge", "Greenwich"]
+function test(){
+    var super_array =[];
+    for (var i=1;i<=3;i++){
+        var subarray=[];
+        for (var u=1;u<=4-i;u++){
+            sub_array.push(u);
+            super_array.push(subarray);
+        }
+    }
+    alert(super_array);
+}
+
 */
